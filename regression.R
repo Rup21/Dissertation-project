7a9281bcd5c59bcf9b21f68dee1fb87d4684f2b7
Biswarup Majumdar <100957055+Rup21@users.noreply.github.com>
2024-04-06 20:08:47 +0530

    Add files via upload

--- regression.R
+++ regression.R
@@ -6,15 +6,14 @@ reg.data
 
 y1=Rate_IPC;y2=Rate_SLL
 X1=UER_rural;X2=UER_urban;X3=log(GDP);X4=log(NSDP);X5=CPI_gen;X6=abs(CPI_food)
-data=data.frame(y1,y2,X1,X2,X3,X4,X5,X6);data
+data=data.frame(y1,y2,X1,X2,X3,X4,X5,X6,X7,X8);data
 
 # raw data regression 
-model1 = lm(y1~ X1+X2+X3+X4+X5+X6);model1;summary(model1)
-model2 = lm(y2~ X1+X2+X3+X4+X5+X6);model2;summary(model2)
+model1 = lm(y1~ X1+X2+X3+X4+X5+X6+X7+X8);model1;summary(model1)
+model2 = lm(y2~ X1+X2+X3+X4+X5+X6+X7+X8);model2;summary(model2)
 
 
 # final regression
-data=cbind(data,X7,X8)
 data1=data[-31,-2];data2= data[,-1]
 data1 
 model_1= lm(y1~X3,data=data1);model_1;summary(model_1)
